6bae022ab9239f35654ee024639f8bf25850ed36
Stefan Miklosovic <stefan.miklosovic@instaclustr.com>
2020-10-22 00:08:50 +0200

    released version 1.0.1

--- cassandra-2.2/src/test/java/com/instaclustr/cassandra/ldap/auth/Cassandra22LDAPIntegrationTest.java
+++ cassandra-2.2/src/test/java/com/instaclustr/cassandra/ldap/auth/Cassandra22LDAPIntegrationTest.java
@@ -41,7 +41,7 @@ public class Cassandra22LDAPIntegrationTest extends AbstractLDAPTesting
     {
         File[] singleFile = Maven.resolver()
             .loadPomFromFile("pom.xml")
-            .resolve("com.instaclustr:cassandra-ldap-2.2:1.0.1-SNAPSHOT")
+            .resolve("com.instaclustr:cassandra-ldap-2.2:1.0.1")
             .withTransitivity()
             .asFile();
 
