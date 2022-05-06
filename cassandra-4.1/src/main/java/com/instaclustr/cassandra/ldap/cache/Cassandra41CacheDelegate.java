092a1708970da94dc9dc97804a12dce10f94c167
Stefan Miklosovic <smiklosovic@apache.org>
2022-05-06 13:18:59 +0200

    update to Cassandra 4.1

---
+++ cassandra-4.1/src/main/java/com/instaclustr/cassandra/ldap/cache/Cassandra41CacheDelegate.java
@@ -0,0 +1,106 @@
+package com.instaclustr.cassandra.ldap.cache;
+
+import java.util.HashMap;
+import java.util.function.Function;
+
+import com.instaclustr.cassandra.ldap.User;
+import org.apache.cassandra.auth.AuthCache;
+import org.apache.cassandra.config.DatabaseDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Cassandra41CacheDelegate implements CacheDelegate
+{
+
+    private static final Logger logger = LoggerFactory.getLogger(Cassandra41CacheDelegate.class);
+
+    private AuthCache<User, User> cassandraCache;
+    private AuthCache<User, User> ldapCache;
+
+    @Override
+    public void invalidate(final User user)
+    {
+        assert cassandraCache != null;
+        this.cassandraCache.invalidate(user);
+        this.ldapCache.invalidate(user);
+    }
+
+    @Override
+    public User get(final User user)
+    {
+        assert cassandraCache != null;
+        assert ldapCache != null;
+
+        try
+        {
+            try
+            {
+                User cassandraUser = this.cassandraCache.get(user);
+
+                if (cassandraUser != null)
+                {
+                    logger.info("Fetching user from Cassandra: " + user.toString());
+                    return cassandraUser;
+                }
+            } catch (final Exception ex)
+            {
+                logger.info("{} not found in Cassandra", user);
+            }
+
+            User ldapUser = this.ldapCache.get(user);
+
+            logger.debug("{} fetched user from LDAP", ldapUser);
+
+            return ldapUser;
+        } catch (final Exception ex)
+        {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public void init(final Function<User, User> cassandraLoadingFunction,
+                     final Function<User, User> ldapLoadingFunction,
+                     final boolean enableCache)
+    {
+        if (this.cassandraCache != null && this.ldapCache != null)
+        {
+            return;
+        }
+
+        this.cassandraCache = new CredentialsCache(cassandraLoadingFunction, "CredentialsCache", enableCache);
+        this.ldapCache = new CredentialsCache(ldapLoadingFunction, "LdapCredentialsCache", enableCache);
+    }
+
+    private static class CredentialsCache extends AuthCache<User, User> implements CredentialsCacheMBean
+    {
+
+        private static final Logger logger = LoggerFactory.getLogger(CredentialsCache.class);
+
+        public CredentialsCache(Function<User, User> loadingFunction, String cacheName, boolean enableCache)
+        {
+            super(cacheName,
+                  DatabaseDescriptor::setCredentialsValidity,
+                  DatabaseDescriptor::getCredentialsValidity,
+                  DatabaseDescriptor::setCredentialsUpdateInterval,
+                  DatabaseDescriptor::getCredentialsUpdateInterval,
+                  DatabaseDescriptor::setCredentialsCacheMaxEntries,
+                  DatabaseDescriptor::getCredentialsCacheMaxEntries,
+                  DatabaseDescriptor::setCredentialsCacheActiveUpdate,
+                  DatabaseDescriptor::getCredentialsCacheActiveUpdate,
+                  loadingFunction,
+                  HashMap::new,
+                  () ->
+                  {
+                      logger.info(String.format("Using cache %s, enabled: %s", cacheName, enableCache));
+
+                      return enableCache;
+                  });
+        }
+
+        public void invalidateCredentials(String username)
+        {
+            invalidate(new User(username));
+        }
+    }
+}
---
+++ cassandra-4.1/src/test/java/com/instaclustr/cassandra/ldap/auth/Cassandra41LDAPIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.instaclustr.cassandra.ldap.auth;
+
+import org.testng.annotations.Test;
+
+public class Cassandra41LDAPIntegrationTest extends AbstractLDAPTest {
+    @Override
+    public String getCassandraVersion() {
+        return System.getProperty("cassandra4.version", "4.1.0");
+    }
+
+    @Override
+    public String getImplementationGAV() {
+        return "com.instaclustr:cassandra-ldap-" + getCassandraVersion() + ":1.0.0";
+    }
+
+    @Test
+    public void ldapTest() throws Exception {
+        super.testLDAPinternal();
+    }
+}
