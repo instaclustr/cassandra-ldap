13751c3881e97e4388df23bb66c3ab15f92c628f
Stefan Miklosovic <smiklosovic@apache.org>
2024-09-09 09:18:19 +0200

    add cassandra-5.0 module

---
+++ cassandra-5.0/src/test/java/com/instaclustr/cassandra/ldap/auth/Cassandra50LDAPIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.instaclustr.cassandra.ldap.auth;
+
+import org.testng.annotations.Test;
+
+public class Cassandra50LDAPIntegrationTest extends AbstractLDAPTest {
+    @Override
+    public String getCassandraVersion() {
+        return System.getProperty("version.cassandra50", "5.0.0");
+    }
+
+    @Override
+    public String getImplementationGAV() {
+        return "com.instaclustr:cassandra-ldap-" + getCassandraVersion() + ":1.0.0";
+    }
+
+    @Test
+    public void ldapTest() throws Exception {
+        super.testLDAPinternal();
+    }
+}
