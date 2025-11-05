f49c759b7223c7185adeca4feee5a66b8db6b656
Stefan Miklosovic <smiklosovic@apache.org>
2025-11-05 16:06:57 +0100

    update to 4.1.10

--- cassandra-4.1/src/test/java/com/instaclustr/cassandra/ldap/auth/Cassandra41LDAPIntegrationTest.java
+++ cassandra-4.1/src/test/java/com/instaclustr/cassandra/ldap/auth/Cassandra41LDAPIntegrationTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class Cassandra41LDAPIntegrationTest extends AbstractLDAPTest {
     @Override
     public String getCassandraVersion() {
-        return System.getProperty("version.cassandra41", "4.1.8");
+        return System.getProperty("version.cassandra41", "4.1.10");
     }
 
     @Override
